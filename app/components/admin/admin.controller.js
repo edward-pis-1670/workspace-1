923d4ae3eccfaee1dcc8d035922107769c4a3c26
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2021-06-08 17:47:51 +0700

    add getUsersByAdmin

--- app/components/admin/admin.controller.js
+++ app/components/admin/admin.controller.js
@@ -4,12 +4,13 @@ const Course = db.courses;
 const Genre = db.genres;
 const Subgenre = db.subGenres;
 const Lecture = db.lectures;
+const { Op } = require("sequelize");
 
 exports.getCourseByAdmin = async (req, res, next) => {
   let condition = {};
   if (req.body.searchQuery)
     condition.name = { [Op.substring]: "%" + req.body.searchQuery + "%" };
-  let sort=[];
+  let sort = [];
   if (!req.body.sort) sort = ["name", "ASC"];
   else {
     switch (parseInt(req.body.sort)) {
@@ -35,3 +36,88 @@ exports.getCourseByAdmin = async (req, res, next) => {
   });
   res.send({ code: 200, courses: data });
 };
+
+exports.getUsersByAdmin = async (req, res, next) => {
+  let condition = {};
+  if (req.body.searchQuery) {
+    condition.username = {
+      [Op.substring]: "%" + req.body.searchQuery + "%",
+    };
+    condition.email = {
+      [Op.substring]: "%" + req.body.searchQuery + "%",
+    };
+  }
+  let sort = [];
+  if (!req.body.sort) sort = ["username", "ASC"];
+  else {
+    switch (parseInt(req.body.sort)) {
+      case 1:
+        sort = ["username", "ASC"];
+        break;
+      case 2:
+        sort = ["username", "DESC"];
+        break;
+    }
+  }
+  const data = await User.findAll({
+    // where: { [Op.or]:condition },
+    where: condition,
+    limit: 8,
+    order: [sort],
+    offset: (req.body.page || 1) * 8 - 8,
+  });
+  res.send({ code: 200, users: data });
+};
+
+// exports.getReviewsCourseByAdmin = async (req, res, next) => {
+//   let condition = { review: true };
+//   if (req.body.searchQuery) {
+//     condition.name = {
+//       [Op.substring]: "%" + req.body.searchQuery + "%",
+//     };
+//   }
+//   let sort = [];
+//   if (!req.body.sort) sort = ["name", "ASC"];
+//   else {
+//     switch (parseInt(req.body.sort)) {
+//       case 1:
+//         sort = ["name", "ASC"];
+//         break;
+//       case 2:
+//         sort = ["name", "DESC"];
+//         break;
+//     }
+//   }
+//   const data = await Course.findAll({
+//     where: condition,
+//     include: [
+//       { model: User, as: "lecturer", attributes: ["_id", "username"] },
+//       { model: Genre, as: "genre" , attributes: ["_id", "name"]},
+//       { model: Subgenre, as: "subgenre",attributes: ["_id", "name"] },
+//       { model: Lecture, as: "lectures", attributes: ["_id", "name"] },
+//     ],
+//     limit: 8,
+//     order: [sort],
+//     offset: (req.body.page || 1) * 8 - 8,
+//   });
+//   res.send({code: 200, courses: data})
+// };
+
+
+// exports.editUserByAdmin = async (req, res) => {
+//   await User.update({username: req.body.username,
+//     email: req.body.email,
+//     verified: true,
+//     role: req.body.role,
+//     creditbalance: req.body.creditbalance,
+//     website: req.body.website,
+//     linkedin: req.body.linkedin,
+//     youtube: req.body.youtube,
+//     twitter: req.body.twitter}, {where:{_id:req.body._id}})
+//     res.send({code:200})
+// }
+
+// exports.deleteUserByAdmin = async (req, res) => {
+// await User.destroy({where:{_id:req.body._id}})
+// res.send({code:200})
+// }
--- app/components/admin/admin.route.js
+++ app/components/admin/admin.route.js
@@ -3,5 +3,9 @@ const express = require("express");
 const router = express.Router()
 
 router.post('/get-courses', controller.getCourseByAdmin)
+router.post('/get-users', controller.getUsersByAdmin)
+// router.post('/get-reviews-course', controller.getReviewsCourseByAdmin)
+// router.post('/edit-user', controller.editUserByAdmin)
+// router.post('/delete-user', controller.deleteUserByAdmin)
 
 module.exports = router
